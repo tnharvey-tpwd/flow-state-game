d4e83e1c1c9cdad8ca84abb214ddbe1cc48a45da
tnharvey-tpwd <timothy.harvey@tpwd.texas.gov>
2026-05-27 12:24:43 -0500

    Created levels.js
    Initial upload of levels file.

---
+++ js/levels.js
@@ -0,0 +1,86 @@
+// The level index where instructor codes are no longer required. 
+// Example: 2 means Level 3 and beyond will bypass the modal.
+const fullUnlockLevel = 2; 
+
+// Pipe exits [Top, Right, Bottom, Left] (1 means open, 0 means closed)
+const pipeDefs = {
+    '|': [1, 0, 1, 0],
+    '-': [0, 1, 0, 1],
+    'L': [1, 1, 0, 0],
+    'F': [0, 1, 1, 0],
+    '7': [0, 0, 1, 1],
+    'J': [1, 0, 0, 1],
+    'T': [0, 1, 1, 1],
+    'S': [0, 0, 1, 0], // Source flows Down
+    'E': [1, 0, 0, 0]  // Exit receives from Top
+};
+
+// SVG graphics mapped to a 0-100 viewBox
+const pipeSVGs = {
+    '|': `<line x1="50" y1="0" x2="50" y2="100" />`,
+    '-': `<line x1="0" y1="50" x2="100" y2="50" />`,
+    'L': `<path d="M 50 0 L 50 50 L 100 50" />`,
+    'F': `<path d="M 100 50 L 50 50 L 50 100" />`,
+    '7': `<path d="M 0 50 L 50 50 L 50 100" />`,
+    'J': `<path d="M 50 0 L 50 50 L 0 50" />`,
+    'T': `<path d="M 0 50 L 100 50 M 50 50 L 50 100" />`,
+    'S': `<circle cx="50" cy="20" r="10" fill="#a6e3a1" stroke="none"/><line x1="50" y1="20" x2="50" y2="100" />`,
+    'E': `<circle cx="50" cy="80" r="10" fill="#f9e2af" stroke="none"/><line x1="50" y1="0" x2="50" y2="80" />`
+};
+
+const levels = [
+    {
+    maxInventoryCapacity: 5,
+    initialInventory: ['|', 'L'],
+    map: [
+            "X X S X X",
+            "X 7 J 7 X",
+            ". F L 7 X",
+            "X . X - X",
+            "X . X | X",
+            "X . . . X",
+            "X X E X X"
+    ]
+    },
+    {
+    maxInventoryCapacity: 5,
+    initialInventory: ['|', 'L'],
+    map: [
+            "X X S X X . . X",
+            "| 7 L 7 L . . X",
+            ". F X T | . . X",
+            "| J X - . . . X",
+            "- 7 X | . . . |",
+            "X . . . . . . X",
+            "X X E X . . . X"
+            ]
+        },
+        {
+    maxInventoryCapacity: 5,
+    initialInventory: ['|', 'L'],
+    map: [
+            "X X S X X . . X",
+            ". . T . . . . .",
+            ". F X 7 | . . X",
+            "| J X - . . . X",
+            "- 7 X | . . . |",
+            "X . . . . . . X",
+            "X X E X . . . X"
+    ]
+    },
+    {
+    maxInventoryCapacity: 5,
+    initialInventory: ['|', 'L'],
+    map: [
+            "X X S X X . . X",
+            ". . . . . . . .",
+            ". F X 7 | . . X",
+            "| J X - . . . X",
+            ". . . . . . . .",
+            "- T X | . . . |",
+            "X . . . . . . X",
+            ". . . . . . . .",
+            "X X E X . . . X"
+    ]
+    }
+];
